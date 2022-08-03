0cd1cbc04df7fa19dcec203872d52d685ecfa529
Angela Yang <kyan211@mywhitecliffe.com>
2022-08-04 01:08:13 +1200

    first commit

---
+++ WeatherApi/Program.cs
@@ -0,0 +1,38 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerDocument(options =>
+{
+    options.DocumentName = "My Amazing API";
+    //options.Version = "V1";
+});
+builder.Services.AddHttpClient("reddit", configureClient: client =>
+{
+    client.BaseAddress = new Uri("https://www.reddit.com/dev/api");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+app.UseOpenApi();
+app.UseSwaggerUi3();
+    
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
